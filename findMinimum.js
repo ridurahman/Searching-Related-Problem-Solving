fe3ee8a40b2951fcbd4a25b171054aa1875573f0
ridurahman <lindaridu@gmail.com>
2024-01-22 19:58:52 +0600

    Updated

--- findMinimum.js
+++ findMinimum.js
@@ -1,11 +1,11 @@
-function findMinimum(nums){
-    let minElement = nums[0];
+function findMinimum(nums) {
+  let minElement = nums[0];
   let left = 0;
   let right = nums.length - 1;
   while (left <= right) {
     let middle = parseInt((left + right) / 2);
-    if (parseInt(nums[middle]) < parseInt(nums[middle-1])) {
-        minElement = nums[middle];
+    if (parseInt(nums[middle]) < parseInt(nums[middle - 1])) {
+      minElement = nums[middle];
       break;
     } else if (parseInt(nums[middle]) > parseInt(nums[right])) {
       left = middle + 1;
@@ -16,14 +16,14 @@ function findMinimum(nums){
   return minElement;
 }
 
-let nums = [3,4,5,1,2];
+let nums = [3, 4, 5, 1, 2];
 console.log(findMinimum(nums));
 
-let nums2 =  [4,5,6,7,0,1,2];
+let nums2 = [4, 5, 6, 7, 0, 1, 2];
 console.log(findMinimum(nums2));
 
-let nums3 = [11,13,15,17];
+let nums3 = [11, 13, 15, 17];
 console.log(findMinimum(nums3));
 
-let nums4 =  [4,5,1,2,3];
+let nums4 = [4, 5, 1, 2, 3];
 console.log(findMinimum(nums4));
